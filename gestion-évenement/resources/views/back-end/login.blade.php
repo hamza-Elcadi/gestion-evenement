bca743915f6123eb63a3157b55bd8394ccab1e7d
hamza-Elcadi <hamzaElcadi@outlook.com>
2023-05-27 15:59:06 +0100

    gg

--- gestion-évenement/resources/views/back-end/login.blade.php
+++ gestion-évenement/resources/views/back-end/login.blade.php
@@ -12,8 +12,8 @@
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
   <!-- CSS Files -->
-  <link href="{{ asset('../public/back-end/login/assets/css/bootstrap.min.css') }}" rel="stylesheet" />
-  <link href="{{ asset('../public/back-end/login/assets/css/paper-dashboard.css?v=2.0.1') }}" rel="stylesheet" />
+  <link href="{{ asset('back-end/login/assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+  <link href="{{ asset('back-end/login/assets/css/paper-dashboard.css?v=2.0.1') }}" rel="stylesheet" />
 </head>
 
 <body class="" style="background-color: #f4f3ef;">
@@ -56,19 +56,19 @@
     </div>
   </div>
   <!--   Core JS Files   -->
-  <script src="{{ asset('../public/back-end/login/assets/js/core/jquery.min.js') }}"></script>
-  <script src="{{ asset('../public/back-end/login/assets/js/core/popper.min.js') }}"></script>
-  <script src="{{ asset('../public/back-end/login/assets/js/core/bootstrap.min.js') }}"></script>
-  <script src="{{ asset('../public/back-end/login/assets/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+  <script src="{{ asset('back-end/login/assets/js/core/jquery.min.js') }}"></script>
+  <script src="{{ asset('back-end/login/assets/js/core/popper.min.js') }}"></script>
+  <script src="{{ asset('back-end/login/assets/js/core/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('back-end/login/assets/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
   <!--  Google Maps Plugin    -->
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!-- Chart JS -->
-  <script src="{{ asset('../public/back-end/login/assets/js/plugins/chartjs.min.js') }}"></script>
+  <script src="{{ asset('back-end/login/assets/js/plugins/chartjs.min.js') }}"></script>
   <!--  Notifications Plugin    -->
-  <script src="{{ asset('../public/back-end/login/assets/js/plugins/bootstrap-notify.js') }}"></script>
+  <script src="{{ asset('back-end/login/assets/js/plugins/bootstrap-notify.js') }}"></script>
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="{{ asset('../public/back-end/login/assets/js/paper-dashboard.min.js?v=2.0.1') }}" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
-  <script src="{{ asset('../public/back-end/login/assets/demo/demo.js') }}"></script>
+  <script src="{{ asset('back-end/login/assets/js/paper-dashboard.min.js?v=2.0.1') }}" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+  <script src="{{ asset('back-end/login/assets/demo/demo.js') }}"></script>
 </body>
 
 </html>
