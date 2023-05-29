01701ff964d04c62cbaa0f800152825f36537d91
hamza-Elcadi <hamzaElcadi@outlook.com>
2023-05-29 21:54:24 +0100

    commit 003

---
+++ gestion-évenement/database/migrations/2023_05_29_203239_create_partners_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->increments('id_partner');
+            $table->string('name_partner');
+            $table->text('description_partner');
+            $table->string('image_partner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('partners');
+    }
+};
---
+++ gestion-évenement/database/migrations/2023_05_29_203617_create_organizers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organizers', function (Blueprint $table) {
+            $table->increments('id_organizer');
+            $table->string('name_organizer');
+            $table->text('description_organizer');
+            $table->string('tel_organizer');
+            $table->string('logo_organizer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('organizers');
+    }
+};
