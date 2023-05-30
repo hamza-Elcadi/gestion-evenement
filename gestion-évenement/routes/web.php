eab9fd81c487daeef387f15e3c86999b396a5032
hamza-Elcadi <hamzaElcadi@outlook.com>
2023-05-30 15:37:39 +0100

    commit 009

--- gestion-évenement/routes/web.php
+++ gestion-évenement/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('back-end.dashboard');
+    return view('welcome');
 });
+Route::get('/admin', [loginController::class,'index']);
