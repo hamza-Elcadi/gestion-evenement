024395c73994888eeef53bf0f507cb4d524b6bf0
hamza-Elcadi <hamzaElcadi@outlook.com>
2023-06-02 01:25:01 +0100

    commit 018

--- gestion-évenement/resources/views/back_end/dashboard.blade.php
+++ gestion-évenement/resources/views/back_end/dashboard.blade.php
@@ -8,13 +8,13 @@
                 <div class="row">
                   <div class="col-5 col-md-4">
                     <div class="icon-big text-center icon-warning">
-                      <i class="nc-icon nc-globe text-warning"></i>
+                      <i class="nc-icon nc-badge text-warning"></i>
                     </div>
                   </div>
                   <div class="col-7 col-md-8">
                     <div class="numbers">
-                      <p class="card-category">Capacity</p>
-                      <p class="card-title">150GB<p>
+                      <p class="card-category">Modulisateur</p>
+                      <p class="card-title">0<p>
                     </div>
                   </div>
                 </div>
@@ -39,8 +39,8 @@
                   </div>
                   <div class="col-7 col-md-8">
                     <div class="numbers">
-                      <p class="card-category">Revenue</p>
-                      <p class="card-title">$ 1,345<p>
+                      <p class="card-category">Events</p>
+                      <p class="card-title">0<p>
                     </div>
                   </div>
                 </div>
@@ -60,13 +60,13 @@
                 <div class="row">
                   <div class="col-5 col-md-4">
                     <div class="icon-big text-center icon-warning">
-                      <i class="nc-icon nc-vector text-danger"></i>
+                      <i class="nc-icon nc-email-85 text-danger"></i>
                     </div>
                   </div>
                   <div class="col-7 col-md-8">
                     <div class="numbers">
-                      <p class="card-category">Errors</p>
-                      <p class="card-title">23<p>
+                      <p class="card-category">Users request</p>
+                      <p class="card-title">0<p>
                     </div>
                   </div>
                 </div>
@@ -86,13 +86,13 @@
                 <div class="row">
                   <div class="col-5 col-md-4">
                     <div class="icon-big text-center icon-warning">
-                      <i class="nc-icon nc-favourite-28 text-primary"></i>
+                      <i class="nc-icon nc-bullet-list-67 text-primary"></i>
                     </div>
                   </div>
                   <div class="col-7 col-md-8">
                     <div class="numbers">
-                      <p class="card-category">Followers</p>
-                      <p class="card-title">+45K<p>
+                      <p class="card-category">Categories</p>
+                      <p class="card-title">0<p>
                     </div>
                   </div>
                 </div>
@@ -115,7 +115,8 @@
                 <p class="card-category">24 Hours performance</p>
               </div>
               <div class="card-body ">
-                <canvas id=chartHours width="400" height="100"></canvas>
+                <canvas id=chartHours width="400" height="100" style="display: none;"></canvas>
+                <canvas id="speedChart" width="400" height="100"></canvas>
               </div>
               <div class="card-footer ">
                 <hr>
@@ -127,14 +128,14 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-12">
             <div class="card ">
               <div class="card-header ">
                 <h5 class="card-title">Email Statistics</h5>
                 <p class="card-category">Last Campaign Performance</p>
               </div>
               <div class="card-body ">
-                <canvas id="chartEmail"></canvas>
+                <canvas id="chartEmail" style="display:none;"></canvas>
               </div>
               <div class="card-footer ">
                 <div class="legend">
@@ -150,26 +151,7 @@
               </div>
             </div>
           </div>
-          <div class="col-md-8">
-            <div class="card card-chart">
-              <div class="card-header">
-                <h5 class="card-title">NASDAQ: AAPL</h5>
-                <p class="card-category">Line Chart with Points</p>
-              </div>
-              <div class="card-body">
-                <canvas id="speedChart" width="400" height="100"></canvas>
-              </div>
-              <div class="card-footer">
-                <div class="chart-legend">
-                  <i class="fa fa-circle text-info"></i> Tesla Model S
-                  <i class="fa fa-circle text-warning"></i> BMW 5 Series
-                </div>
-                <hr />
-                <div class="card-stats">
-                  <i class="fa fa-check"></i> Data information certified
-              </div>
-            </div>
-          </div>
+
         </div>
     </div>
 </div>
